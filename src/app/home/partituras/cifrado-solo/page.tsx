af2a138303226dd1725e6038aeb26913f2093616
RudeusGrayrat <miguelnc2502@gmail.com>
2024-06-05 14:56:23 -0500

    pruebaOfLanding

--- src/app/home/partituras/cifrado-solo/page.tsx
+++ src/app/home/partituras/cifrado-solo/page.tsx
@@ -1,20 +1,8 @@
 "use client"
-import axios from "axios"
-import { useEffect, useState } from "react"
 import Cards from "@/components/Cards/Cards"
 
 export default function LetraYAcorde() {
-    const [response, setResponse] = useState([])
-    const pdf = async () => {
-        const response = await axios.get("https://drive.google.com/file/d/1Yqa_mbrOOYcrQUH40OlYHBQlMRA-Ac3d/view?usp=sharing")
-        setResponse(response.data)
-    }
-    useEffect(() => {
-        pdf()
-    }, [])
-
-    console.log(response);
-
+    
 
     return (
         <div className="flex flex-wrap my-20 mx-36 justify-center items-center ">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
-import Image from 'next/image';
 
 export default function Home() {
 
   return (
-    <div className="flex  items-center justify-center p-24 h-[100%]  bg-slate-100">
-      <Image alt='Logo' width={400} height={200} src="/imagenPartitura.jpeg"></Image>
-      <div className='m-8 text-center'>
+    <div className="flex flex-col items-start justify-center p-24 h-[100%]
+      bg-slate-100 bg-[url('/imagenPartitura.jpeg')] bg-cover ">
+      <div className='m-8 text-center bg-slate-400 p-2 px-6 rounded-lg'>
         <h1 className=' text-5xl'>Bienbenido</h1>
         <span>Quienes somos?
           <br />somos blababallabalba
           <br /> labablabablabblablbalblabl
           <br /> labablabablabblablbalblabl
-          <br /> labablabablabblablbalblabl
-          <br /> labablabablabblablbalblabl</span>
+        </span>
+      </div>
+      <div className='text-center mx-16' >
+        <button className='m-2 bg-slate-500  p-2 rounded-lg'>Conocer</button>
+        <button className='m-2 bg-slate-500  p-2 rounded-lg'>Contactar</button>
       </div>
     </div >
   );

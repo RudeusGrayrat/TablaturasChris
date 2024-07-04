c5960d65527e9d633c284d745f17a6e4e2e76fe3
RudeusGrayrat <miguelnc2502@gmail.com>
2024-07-03 21:05:52 -0500

    erros

--- src/app/api/saludar/saludar.ts
+++
@@ -1,6 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default function saludar(req: NextApiRequest, res: NextApiResponse){
-    res.status(200).json({message: "Hola mundo desde el servidor"})
-    
-}
